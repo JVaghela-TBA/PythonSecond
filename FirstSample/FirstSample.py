e3fcbc33c4ce81c3f231bf37c25286989ee0643b
VCMPARTNERS\jvaghela <jvaghela@thebeastapps.com>
2017-07-24 12:01:23 +0530

    object deletion in python

--- FirstSample/FirstSample.py
+++ FirstSample/FirstSample.py
@@ -13,6 +13,13 @@ try:
     samsung=Phone.Samsung("Galaxy S",47000);
     samsung.print();
 
+    #Print Class Variables
+    #print("Samsung.__doc__ :"+str(Phone.Samsung.__doc__));
+    #print("Samsung.__name__ :"+str(Phone.Samsung.__name__));
+    #print("Samsung.__module__ :"+str(Phone.Samsung.__module__));
+    #print("Samsung.__bases__ :"+str(Phone.Samsung.__bases__));
+    #print("Samsung.__dict__ :"+str(Phone.Samsung.__dict__));
+
     #using attribute method of python
     #print(hasattr(samsung,"price"));
     #setattr(samsung,"price",50000);
@@ -24,6 +31,7 @@ try:
 except Exception as e:
     print("Exception Found"+str(e));
 finally:
+    
     del(phone5s);
     del(phone6s);
     del(samsung);
